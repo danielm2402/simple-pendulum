dbdbf55cf6a2cfa56d0b8c73f0faeb540e09c548
danielm2402 <damunoz216@unicauca.edu.co>
2020-09-13 22:11:55 -0500

    canvas section ag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/header/Header'
 import LandingPage from './pages/landing/Landing'
-
+import Platform from './pages/platform/Platform'
 function App() {
   return (
     <div>
       <Header/>
       <LandingPage/>
+      <Platform/>
     </div>
   );
 }
---
+++ src/pages/platform/Platform.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Platform.css'
+export default function Platform() {
+    return (
+        <div className="page-platform">
+            <div className="platform-header">
+                <div className="platform-tools">
+                </div>
+                <div className="platform-draw">
+                </div>
+                <div className="platform-inputs">
+                </div>
+            </div>
+            <div className="platform-canvas">
+                <div className="platform-tools">
+                </div>
+                <div className="platform-draw">
+                </div>
+                <div className="platform-inputs">
+                </div>
+            </div>
+        </div>
+    )
+}
