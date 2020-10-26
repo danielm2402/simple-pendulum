604484c5600acec5a1f989ad865f4e804a259e6c
danielm2402 <damunoz216@unicauca.edu.co>
2020-10-25 20:11:23 -0500

    Updates

---
+++ precache-manifest.94c540374a82b440b6af4dee884b9e95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed78e916b3eca2cebf800cee3d7f7c3",
+    "url": "/simple-pendulum/index.html"
+  },
+  {
+    "revision": "c329e9ebd3f46e25c2c4",
+    "url": "/simple-pendulum/static/css/main.5d4fe5e0.chunk.css"
+  },
+  {
+    "revision": "7c8759b2e0330322643e",
+    "url": "/simple-pendulum/static/js/2.e8c4a3bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-pendulum/static/js/2.e8c4a3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c329e9ebd3f46e25c2c4",
+    "url": "/simple-pendulum/static/js/main.5582b861.chunk.js"
+  },
+  {
+    "revision": "01c35be9f93907588d51",
+    "url": "/simple-pendulum/static/js/runtime-main.c7ef954c.js"
+  },
+  {
+    "revision": "e46a7579c0d4713e95b3e785cd520d18",
+    "url": "/simple-pendulum/static/media/bg1.e46a7579.jpg"
+  },
+  {
+    "revision": "46c908607ed4aa89ef4da2ebea7735d9",
+    "url": "/simple-pendulum/static/media/gif-pendulum.46c90860.gif"
+  }
+]);
