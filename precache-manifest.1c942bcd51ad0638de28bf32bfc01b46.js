4772487c4acdf8c4aeadede24d0c9ed8477c8a2a
danielm2402 <damunoz216@unicauca.edu.co>
2020-11-06 17:28:02 -0500

    Updates

--- precache-manifest.1c942bcd51ad0638de28bf32bfc01b46.js
+++ precache-manifest.1c942bcd51ad0638de28bf32bfc01b46.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11cb76e91ed4f834ae702170716f0cd0",
+    "revision": "05896fa91b2b6e250841871601b17750",
     "url": "/simple-pendulum/index.html"
   },
   {
-    "revision": "3943c3ef2a4d2019f3c5",
+    "revision": "7865e23c268cb575d66d",
     "url": "/simple-pendulum/static/css/main.61ac6113.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/simple-pendulum/static/js/2.b6067367.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3943c3ef2a4d2019f3c5",
-    "url": "/simple-pendulum/static/js/main.0a6ae684.chunk.js"
+    "revision": "7865e23c268cb575d66d",
+    "url": "/simple-pendulum/static/js/main.edaee699.chunk.js"
   },
   {
     "revision": "01c35be9f93907588d51",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-pendulum/precache-manifest.ef7842b1f9de6c935dd3084016517051.js"
+  "/simple-pendulum/precache-manifest.1c942bcd51ad0638de28bf32bfc01b46.js"
 );
 
 self.addEventListener('message', (event) => {
